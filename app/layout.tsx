65820a6365201b03ff5a98714478d5154b65f798
rgerum <14153051+rgerum@users.noreply.github.com>
2025-03-26 11:43:36 -0400

    added docs in build process

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Saenopy - 3D traction force microscopy',
-  description: 'An open-source software for calculating 3D traction forces in cell mechanics research',
-}
+  title: "Saenopy - 3D traction force microscopy",
+  description:
+    "An open-source software for calculating 3D traction forces in cell mechanics research",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
